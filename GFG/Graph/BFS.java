f631f9d4f24701c50d74fbad3dd9dd46fbb15f94
HARSH RAJ <hr45226@gmail.com>
2021-06-19 07:46:03 +0530

    Breadth First Search

---
+++ GFG/Graph/BFS.java
@@ -0,0 +1,71 @@
+package GFG.Graph;
+import java.io.*;
+import java.util.*;
+import java.util.ArrayList;
+
+public class BFS {
+    static class Edge {
+        int src;
+        int nbr;
+  
+        Edge(int src, int nbr) {
+           this.src = src;
+           this.nbr = nbr;
+        }
+     }
+  
+     public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  
+        int vtces = Integer.parseInt(br.readLine());
+        ArrayList<Edge>[] graph = new ArrayList[vtces];
+        for (int i = 0; i < vtces; i++) {
+           graph[i] = new ArrayList<>();
+        }
+  
+        int edges = Integer.parseInt(br.readLine());
+        for (int i = 0; i < edges; i++) {
+           String[] parts = br.readLine().split(" ");
+           int v1 = Integer.parseInt(parts[0]);
+           int v2 = Integer.parseInt(parts[1]);
+           graph[v1].add(new Edge(v1, v2));
+           graph[v2].add(new Edge(v2, v1));
+        }
+  
+        int src = Integer.parseInt(br.readLine());
+
+        ArrayDeque<Pair> dq =new ArrayDeque<>();
+        dq.add(new Pair(src ,src +""));
+        boolean[] visited =new boolean[vtces];
+
+        while(dq.size()>0){
+            Pair rem =dq.removeFirst();
+            if(visited[rem.v]==true){
+                continue;
+            }
+
+            visited[rem.v] =true;
+            System.out.println(rem.v + "@" + rem.psf);
+
+            for(Edge e :  graph[rem.v]){
+                if(visited[e.nbr]==false){
+                    dq.add(new Pair(e.nbr ,rem.psf +e.nbr));
+                }
+            }
+        }
+  
+        // write your code here  
+     }
+    public static class Pair {
+        int v ;
+        String psf;
+
+        Pair(int v ,String psf){
+            this.v =v;
+            this.psf =psf;
+        }
+    }
+
+
+    
+}
