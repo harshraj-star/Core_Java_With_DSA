1ca725a47e17a92f00b850d53e2701bdbeab49a0
HARSH RAJ <hr45226@gmail.com>
2021-06-19 08:30:52 +0530

    Is Graph Cyclic...?

---
+++ GFG/Graph/CyclicGraph.java
@@ -0,0 +1,86 @@
+package GFG.Graph;
+import java.io.*;
+import java.util.*;
+import java.util.ArrayList;
+
+public class CyclicGraph {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+
+      boolean[] visited =new boolean[vtces];
+
+      for(int v=0; v<vtces ; v++){
+          if(visited[v]==false){
+              boolean cycle = isGraphCyclic(graph ,v ,visited);
+              if(cycle){
+                  System.out.println("true");
+                            }
+          }
+          
+      }
+
+      System.out.println("false");
+
+      // write your code here
+   }
+
+   public static boolean isGraphCyclic(ArrayList<Edge> [] graph ,int src ,boolean[] visited){
+       ArrayDeque<Pair> dq =new ArrayDeque<>();
+       dq.add(new Pair(src ,src +""));
+       while(dq.size()>0){
+           Pair rem = dq.removeFirst();
+
+        if(visited[rem.v] ==true){
+            return true;
+        }
+        visited[rem.v] =true;
+
+        for(Edge e : graph[rem.v]){
+            if(visited[e.nbr]==false){
+                dq.add(new Pair(e.nbr ,rem.psf + e.nbr));
+            }
+        }
+       }
+
+       return false;
+   }
+
+   public static class Pair{
+       int v;
+       String psf;
+
+       Pair(int v ,String psf){
+           this.v =v;
+           this.psf =psf;
+       }
+
+   }
+}
