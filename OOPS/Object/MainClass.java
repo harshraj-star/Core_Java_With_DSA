85bb2ef31129ce34e15d72179969423e793db26d
HARSH RAJ <hr45226@gmail.com>
2021-07-12 23:35:53 +0530

    oops in java

---
+++ OOPS/Object/MainClass.java
@@ -0,0 +1,15 @@
+package OOPS.Object;
+import java.io.*; 
+
+public class MainClass {
+    cat cat1 = new cat();
+    cat cat2 = new cat();
+
+    cat1.walk();
+    cat1.eat();
+
+    cat2.walk();
+    cat2.eat();
+
+    
+}
---
+++ OOPS/Object/cat.java
@@ -0,0 +1,24 @@
+package OOPS.Object;
+
+public class cat {
+    boolean hasFur;
+    String color ,breed;
+    public static int legs ,eyes;
+
+    public void walk(){
+        System.out.println("cat is walking");
+    }
+
+    public static void eat(){
+        System.out.println("cat is eating");
+    }
+
+    public static void description(){
+        System.out.println("This cat has"+ legs + "legs and "+ eyes+ " eyes" );
+    }
+
+    
+
+    
+}
+
